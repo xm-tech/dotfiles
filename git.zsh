4b60250f907fd32cefe1228ef63c40eafb48d8e4
xm-tech <maxm09@163.com>
2022-12-09 12:18:05 +0800

    conf git icdiff

--- git.zsh
+++ git.zsh
@@ -2,3 +2,4 @@
 git config --global merge.tool vimdiff
 git config --global mergetool.prompt false
 git config --global merge.conflictstyle diff3
+git config --global icdiff.options '--highlight --line-numbers'
