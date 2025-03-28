4f312c6a7d95dbdd398f65c4657593f4b9eca634
xm-tech <maxm09@163.com>
2025-03-28 14:40:23 +0800

    fix ccls

--- ccls_load.sh
+++ ccls_load.sh
@@ -1,14 +1,22 @@
 #!/bin/sh
 
-# Homebrew clang is a bit different from /usr/bin/clang. Invoke clang++ -xc++ -fsyntax-only -v /dev/null to get a list of C/C++ search paths. 
-# then add them as -isystem into the shell script wrapper. For example:
-exec /usr/local/bin/ccls --init='{"clang":{"extraArgs":[
+# Homebrew clang is a bit different from /usr/bin/clang. 
+# This script configures ccls with the correct include paths for macOS.
+# To get your system's include paths, run: clang++ -xc++ -fsyntax-only -v /dev/null
+
+ccls_path=$(brew --prefix ccls)
+exec "${ccls_path}/bin/ccls" --init='{"clang":{"extraArgs":[
   "-std=c11",
   "-isystem/usr/local/include",
   "-isystem/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include/c++/v1",
-  "-isystem/Library/Developer/CommandLineTools/usr/lib/clang/14.0.0/include",
+  "-isystem/Library/Developer/CommandLineTools/usr/lib/clang/16/include",
   "-isystem/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include",
   "-isystem/Library/Developer/CommandLineTools/usr/include",
   "-isystem/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/System/Library/Frameworks"
-]}}' "$@"
-#  "-isystem/usr/local/Cellar/sdl2/2.24.0/include/SDL2"
+]},
+"cache": {
+  "directory": "~/.ccls-cache"
+}}' "$@"
+
+# Uncomment and modify if you need additional library includes:
+# "-isystem/usr/local/Cellar/sdl2/2.24.0/include/SDL2"
